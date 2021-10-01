74d877a49615c9ca5047ad7da645c09f404db08c
Phil Lee <plee@redoxengine.com>
2021-10-01 10:44:00 -0400

    add mdx package and render to render blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,46 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
-const BlogPage = ({data}) => {
-  return (
+const BlogPage = ({ data }) => (
     <Layout pageTitle="My Blog Posts">
-        <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
+        data.allMdx.nodes.map((node) => {
+          const { id, frontmatter, body } = node
+          return (
+            <article key={id}>
+              <h2>{frontmatter.title}</h2>
+              <p>Posted: {frontmatter.date}</p>
+              <MDXRenderer>
+                {body}
+              </MDXRenderer>
+            </article>
+          )
+        })
       }
-      </ul>
     </Layout>
   )
-}
+
 
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+ query MyQuery {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      parent {
+        ... on File {
+          modifiedTime(formatString: "MMMM D, YYYY")
+        }
       }
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      id
+      body
     }
   }
+}
 `
 
 export default BlogPage
