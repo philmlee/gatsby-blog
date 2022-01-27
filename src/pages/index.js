5874efed4683cc7445ad2cc9b3e446289e55c917
Phil Lee <plee@redoxengine.com>
2022-01-27 13:21:01 -0500

    add blog on gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Welcome!">
+      {/* <p>I'm making this by following the Gatsby Tutorial.</p> */}
          <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/journey.jpeg"
